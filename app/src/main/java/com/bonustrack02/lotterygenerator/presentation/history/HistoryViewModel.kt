a0309ecf2a69a17613d7702198f26f527513fcac
jhkim02 <jhkim02@focushns.com>
2025-12-18 11:49:03 +0900

    Feat: Add generation history delete functionality
    Delete a row by id.

--- app/src/main/java/com/bonustrack02/lotterygenerator/presentation/history/HistoryViewModel.kt
+++ app/src/main/java/com/bonustrack02/lotterygenerator/presentation/history/HistoryViewModel.kt
@@ -3,20 +3,29 @@ package com.bonustrack02.lotterygenerator.presentation.history
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bonustrack02.domain.model.GenerationHistory
+import com.bonustrack02.domain.usecase.DeleteGenerationHistoryUseCase
 import com.bonustrack02.domain.usecase.GetGenerationHistoryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HistoryViewModel @Inject constructor(
-    getGenerationHistoryUseCase: GetGenerationHistoryUseCase
+    getGenerationHistoryUseCase: GetGenerationHistoryUseCase,
+    private val deleteGenerationHistoryUseCase: DeleteGenerationHistoryUseCase
 ) : ViewModel() {
     val generationHistories: StateFlow<List<GenerationHistory>> = getGenerationHistoryUseCase().stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5000),
         initialValue = emptyList()
     )
+
+    fun deleteGenerationHistory(id: Int) {
+        viewModelScope.launch {
+            deleteGenerationHistoryUseCase(id)
+        }
+    }
 }
--- domain/src/main/java/com/bonustrack02/domain/repository/GenerationHistoryRepository.kt
+++ domain/src/main/java/com/bonustrack02/domain/repository/GenerationHistoryRepository.kt
@@ -6,6 +6,8 @@ import kotlinx.coroutines.flow.Flow
 interface GenerationHistoryRepository {
     suspend fun saveGenerationHistory(history: GenerationHistory)
 
+    suspend fun deleteGenerationHistory(id: Int)
+
     fun getGenerationHistoryStream(): Flow<List<GenerationHistory>>
 
     suspend fun clearGenerationHistoryTable()
