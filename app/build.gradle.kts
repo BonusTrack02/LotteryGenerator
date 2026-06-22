cd675004ee4fb93941144731bd3dc5cfd7b37883
BonusTrack02 <wlsgud321@gmail.com>
2026-06-22 16:46:07 +0900

    Chore: Update version to 2.10.0 + 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.bonustrack02.lotterygenerator"
         minSdk = 28
         targetSdk = 36
-        versionCode = 15
-        versionName = "2.9.0"
+        versionCode = 16
+        versionName = "2.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
